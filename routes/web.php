95d8a52588d7a6021ba7dbab663361f82b80a6ac
i.zubarev <zubarevski@gmail.com>
2019-05-17 18:10:25 +0300

    clear-cache

--- routes/web.php
+++ routes/web.php
@@ -1,2 +1,12 @@
 <?php
 // Роуты не требующие авторизации!!!
+Route::get('/clear-cache', function () {
+    Artisan::call('cache:clear');
+    Artisan::call('view:clear');
+    Artisan::call('optimize:clear');
+    Artisan::call('config:clear');
+    Artisan::call('route:clear');
+    Artisan::call('storage:link');
+
+    return 'all cache cleared';
+});
