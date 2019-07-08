33427844b8926eb4678102a943661f376a39c843
i.zubarev <zubarevski@gmail.com>
2019-07-08 16:41:58 +0300

    remove unused payment config value

--- config/payment.php
+++ config/payment.php
@@ -6,8 +6,5 @@ return [
         'merchantId' => env('YANDEX_SHOP_ID'),
         'scid'       => env('YANDEX_SCID'),
         'secretKey'  => env('YANDEX_SECRET_KEY'),
-        'successURL' => env('YANDEX_SUCCESS_URL', '/'),
-        'failURL'    => env('YANDEX_FAIL_URL', '/'),
-        'isTest'     => env('PAYMENT_SYSTEM_TEST', true),
     ],
 ];
