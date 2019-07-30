f73a636640bb3ca461a1291773a64ef9b598a104
i.zubarev <zubarevski@gmail.com>
2019-07-30 09:34:52 +0300

    correction mobile logo, chat

--- resources/views/layouts/_app/footer.blade.php
+++ resources/views/layouts/_app/footer.blade.php
@@ -4,6 +4,7 @@
     <div class="pull-right hidden-xs">
         {{--Anything you want--}}
     </div>
+    <a href="https://mera-capital.com/user-agreement" target="_blank" class="f-link">Пользовательское соглашение</a>
     <!-- Default to the left -->
     {{--<strong>Copyright &copy; {{ date( 'Y' ) }} <a href="http://mera-capital.com">MeraCapital</a>.</strong> All rights reserved.--}}
 </footer>
